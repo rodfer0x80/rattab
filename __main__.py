30f4a6b82c99c906c44ed13638de29b3ba9ab5c7
rodfer0x80 <rodfer0x80@proton.me>
2022-12-14 11:47:07 +0000

    Aborting commit due to empty commit message.

--- __main__.py
+++ __main__.py
@@ -13,7 +13,9 @@ import keyboard
 import nltk
 from nltk.corpus import words
 
+
 CSV_SEP = ","
+NEWLINE = "\n"
 
 USER_INPUT_ERROR = -2
 UNKOWN_ERROR = -1
@@ -22,6 +24,45 @@ READ_FILE_ERROR = 1
 WRITE_FILE_ERROR = 2
 APPEND_FILE_ERROR = 4
 DEADLOCK_NOT_FOUND_ERROR = 8 
+
+
+class Utils:
+    def __init__(self):
+        self.debuglog_local = "./debug.log"
+        self.debuglog = Debuglog(self.debuglog_local)
+        return None
+
+    def handleError(self, e, ret=UNKOWN_ERROR):
+        self.debuglog.log(f"[{ret}] {e}")
+        sys.stderr.write(f"[{ret}] {e}")
+        return ret
+    
+    def initFile(self, file_local, mode='a'):
+        if mode == 'a':
+            try:
+                fp = open(file_local, 'a')
+                fp.write("")
+                fp.close()
+                return SUCCESS
+            except:
+                pass
+            try:
+                fp = open(file_local, 'w')
+                fp.write("")
+                fp.close()
+                return SUCCESS
+            except Exception as e:
+                return self.handleError(e, WRITE_FILE_ERROR)
+        elif mode == 'w':
+            try:
+                fp = open(file_local, 'w')
+                fp.write("")
+                fp.close()
+            except Exception as e:
+                return self.handleError(e, WRITE_FILE_ERROR)
+        else:
+            return self.handleError("Wrong file IO mode", USER_INPUT_ERROR)
+
 
 class Deadlock():
     def __init__(self):
@@ -43,6 +84,7 @@ class Deadlock():
             self.lock = not self.lock
             return token
 
+
 class Debuglog():
     def __init__(self, debuglog_local="./debug.log"):
         self.debuglog_local = debuglog_local
@@ -55,65 +97,44 @@ class Debuglog():
         try:
             while not self.deadlock.toggle(self.debuglog_local):
                 _ = 0
-            fp = open(self.debuglog_local, 'a')
             c =  str(datetime.datetime.now()).split('.')[0].replace(' ', '_')
-            fp.write(f"{c}{CSV_SEP}{txt}{sep}")
+            fp.write(f"{c}{CSV_SEP}{txt}{sep}{NEWLINE}")
             fp.close()
             while not self.deadlock.toggle(self.debuglog_local):
                 _ = 0
             return SUCCESS
         except Exception as e:
-            sys.stderr.write(e)
+            sys.stderr.write(f"e")
             return APPEND_FILE_ERROR
 
     def initDebuglog(self):
-        return Utils.initFile(self.debuglog)
-
-
-class Utils:
-    def __init__(self):
-        self.debuglog_local = "./debug.log"
-        self.debuglog = Debuglog(self.debuglog_local)
-        return None
-
-    def initFile(file_local, mode='r'):
-        if mode == 'r':
-            try:
-                fp = open(file_local, 'r')
-                fp.close()
-            except FileNotFoundError:
-                try:
-                    fp = open(file_local, 'w')
-                    fp.write("")
-                    fp.close()
-                except Exception as e:
-                    return handleError(e, WRITE_FILE_ERROR)
-        elif mode == 'w':
-            try:
-                fp = open(file_local, 'w')
-                fp.write("")
-                fp.close()
-            except Exception as e:
-                return handleError(e, WRITE_FILE_ERROR)
-        else:
-            return handleError("Wrong file IO mode", USER_INPUT_ERROR)
-        return SUCCESS
-
-    def handleError(e, ret=UNKOWN_ERROR):
-        sys.stderr.write(e)
-        self.debuglog.debuglog(e)
-        return ret
+        try:
+            fp = open(file_local, 'a')
+            fp.close()
+            return SUCCESS
+        except:
+            pass
+        try:
+            fp = open(file_local, 'w')
+            fp.write("")
+            fp.close()
+            return SUCCESS
+        except Exception as e:
+            sys.stderr.write(f"{e}")
+            return WRITE_FILE_ERROR
 
 
 class RatTab:
     def __init__(self, custom=True):
-        ntlk.download("words")
+        self.utils = Utils()
         self.custom = custom
         self.mode = ""
+        self.dictionary_local = "~/.config/rattab/rattab_wordlist.txt"
+        self.initDictionary()
         self.clearWord()
-        self.startDebuglog()
-        os.seteuid(0)
         self.running = False
+        os.seteuid(0)
+        ntlk.download("words")
 
     def clearWord(self):
         self.word = ""
@@ -126,19 +147,27 @@ class RatTab:
             self.dictionary = fp.readlines()
             fp.close()
             return SUCCESS
-        except FileNotFoundError:
+        except:
+            pass
             self.dictionary = list()
+            self.utils.initFile(self.dictionary_local)
+            return SUCCESS
+
+    def dictionaryAdd(self, word):
         try:
-            initFile(self.dictionary)
+            fp = open(self.dictionary_local, 'a')
+            fp.write(f"{NEWLINE}{self.word}")
+            fp.close()
+            self.dictionary.append(word)
+            return SUCCESS
         except Exception as e:
-            return handleError(e)
-        return SUCCESS
+            return self.utils.handleError(e, APPEND_FILE_ERROR)
 
     def spellcheck(self):
         if len(self.word) > 1:
             self.word = re.sub(r"^[w]", "", self.word.lower())
             if self.word not in words.words():
-                debug.log(f"Invalid Word: {self.word}")
+                print(f"Invalid word {self.word}")
             if self.word not in self.dictionary:
                 self.dictionaryAdd(self.word)
         self.clearWord()
@@ -176,7 +205,6 @@ class RatTab:
         self.running = not self.running
         if self.running:
             self.runme()
-        
         return SUCCESS
 
     def runme(self):
